0a5c506e4459e8aa90ab7ae70aeecdcc95024196
Kamal Kumar <kamalkumar1027@yahoo.com>
2022-03-15 17:27:00 -0400

    just need to add gifs

--- app.js
+++ app.js
@@ -1,66 +1,76 @@
-console.log('testing')
-$(function () {
-	setTimeout(function () {
-		loopFloaters($("#left_inner"));
-	}, getRandomIntegerInRange(0, 3000));
-	setTimeout(function () {
-		loopFloaters($("#right_outer"));
-	}, getRandomIntegerInRange(0, 3000));
-	setTimeout(function () {
-		loopFloaters($("#right_inner"));
-	}, getRandomIntegerInRange(0, 3000));
-	setTimeout(function () {
-		loopFloaters($("#left_outer"));
-	}, getRandomIntegerInRange(0, 3000));
+$(document).ready(function(){
 
-	$(document).on("click touchstart", ".btn-jake", function () { popBubble(this); });
+  function getRandomIntegerInRange(min, max) {
+  	min = Math.ceil(min);
+  	max = Math.floor(max);
+  	return Math.floor(Math.random() * (max - (min + 1)) + min);
+  }
+
+  function loopFloaters($el) {
+  	var desiredBottom = $(window).height();
+  	var desiredTop = -220
+  	var speed = getRandomIntegerInRange(7000, 12000);
+
+  	$el.css({ top: desiredBottom });
+  	$el.removeClass("hide");
+  	$el.animate({ top: desiredTop }, speed, function () {
+  		if (parseInt($("#counter").val(), 10) < 4) {
+  			loopFloaters($el);
+  		}
+  	});
+  }
+
+  function popBubble(el) {
+  	var $el = $(el);
+  	var counter = parseInt($("#counter").val(), 10) + 1;
+  	$("#counter").val(counter);
+
+  	if (counter === 4) {
+  		$(".button").addClass("hide");
+  	} else {
+  		$el.addClass("hide");
+  	}
+
+  	switch (counter) {
+  		case 1:
+  			$("#first").text("Christina and Kumar");
+  			break;
+  		case 2:
+  			$("#second").text("Are expecting");
+  			break;
+  		case 3:
+  			$("#third").text("A beautiful");
+  			break;
+  		case 4:
+  			$("#fourth").text("Girl");
+        $("body").css('background-color', 'pink');
+  			break;
+  	}
+  }
+
+  $(function () {
+  	setTimeout(function () {
+  		loopFloaters($("#left-top"));
+  	}, getRandomIntegerInRange(0, 3000));
+  	setTimeout(function () {
+  		loopFloaters($("#left-center"));
+  	}, getRandomIntegerInRange(0, 3000));
+  	setTimeout(function () {
+  		loopFloaters($("#right-center"));
+  	}, getRandomIntegerInRange(0, 3000));
+  	setTimeout(function () {
+  		loopFloaters($("#right-top"));
+  	}, getRandomIntegerInRange(0, 3000));
+
+    $(document).on("click touchstart", ".button", function () { popBubble(this); });
+
+  });
+
+  $('.button').click(function(){
+    $(this).css('display', 'none')
+  })
+
+
+
+
 });
-
-function loopFloaters($el) {
-	var desiredBottom = $(window).height();
-	var desiredTop = -220
-	var speed = getRandomIntegerInRange(7000, 12000);
-
-	$el.css({ top: desiredBottom });
-	$el.removeClass("hide");
-	$el.animate({ top: desiredTop }, speed, function () {
-		if (parseInt($("#counter").val(), 10) < 4) {
-			loopFloaters($el);
-		}
-	});
-}
-
-function getRandomIntegerInRange(min, max) {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-
-	return Math.floor(Math.random() * (max - (min + 1)) + min);
-}
-
-function popBubble(el) {
-	var $el = $(el);
-	var counter = parseInt($("#counter").val(), 10) + 1;
-	$("#counter").val(counter);
-
-	if (counter === 4) {
-		$(".btn-jake").addClass("hide");
-	} else {
-		$el.addClass("hide");
-	}
-
-	switch (counter) {
-		case 1:
-			$("#first_hint").text("Mommy");
-			break;
-		case 2:
-			$("#second_hint").text("is carrying");
-			break;
-		case 3:
-			$("#third_hint").text("my little");
-			break;
-		case 4:
-			$("#reveal").text("BROTHER!!!").css({ "text-decoration": "underline" });
-			$("body").addClass("bg-primary");
-			break;
-	}
-}
